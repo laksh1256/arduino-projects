7ff3948176d334e061c152194e448060f50545d7
laksh1256 <58773560+laksh1256@users.noreply.github.com>
2020-07-23 15:27:06 +0530

    Create sargam_player.ino

---
+++ sargam_player.ino
@@ -0,0 +1,41 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+tone(8,240,5000);
+delay(1000);
+tone(8,770,1000);
+delay(1000);
+tone(8,810,1000);
+delay(1000);
+tone(8,837.5,1000);
+delay(1000);
+tone(8,860,1000);
+delay(1000);
+tone(8,900,1000);
+delay(1000);
+tone(8,950,1000);
+delay(1000);
+tone(8,980,1000);
+delay(1000);
+delay(2000);tone(8,950,1000);
+delay(1000);
+tone(8,900,1000);
+delay(1000);
+tone(8,860,1000);
+delay(1000);
+tone(8,837.5,1000);
+delay(1000);
+tone(8,800,1000);
+delay(1000);
+tone(8,770,1000);
+delay(1000);
+tone(8,740,1000);
+delay(3000);
+
+
+ 
+}
